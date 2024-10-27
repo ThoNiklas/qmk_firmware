3a42b13eb523ebc25b7f208c14c49abadc98d60a
Niklas Thomas <thomas@ice.rwth-aachen.de>
2024-10-27 16:45:01 +0100

    Fixed keymap, bug in thumb keys

--- keyboards/blavinge/keymaps/ThoNiklas/keymap.c
+++ keyboards/blavinge/keymaps/ThoNiklas/keymap.c
@@ -23,23 +23,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       */
     // Base
     [0] = LAYOUT_ortho_staggered(
-        KC_Q,     KC_W,       KC_F,       KC_P,       KC_B,      LCTL_T(KC_TAB),  
+        KC_Q,     KC_W,       KC_F,       KC_P,       KC_B,      MT(MOD_MEH,KC_ENT), 
         KC_A,     KC_R,       KC_S,       KC_T,       KC_G,      LT(3, KC_SPC),
-        KC_Z,     KC_X,       KC_C,       KC_D,       KC_V,      MT(MOD_MEH,KC_ENT),
+        KC_Z,     KC_X,       KC_C,       KC_D,       KC_V,      LCTL_T(KC_TAB), 
 
-        KC_TAB,   KC_Y,       KC_U,       KC_L,       KC_J,      LGUI_T(KC_ESCAPE), 
+        KC_TAB,   KC_Y,       KC_U,       KC_L,       KC_J,      KC_LSFT,
         KC_O,     KC_I,       KC_E,       KC_N,       KC_M,      LT(4, KC_BSPC),
-        KC_SCLN,  MO(2),      MO(1),      KC_H,       KC_K,      KC_LSFT                          
+        KC_SCLN,  MO(2),      MO(1),      KC_H,       KC_K,      LGUI_T(KC_ESCAPE)                           
     ),
     // Sym
     [1] = LAYOUT_ortho_staggered(
-        KC_GT,    KC_RCBR,    KC_RBRC,    KC_RPRN,    KC_BSLS,   KC_UNDS,                 
+        KC_GT,    KC_RCBR,    KC_RBRC,    KC_RPRN,    KC_BSLS,   KC_DQUO,                
         KC_LT,    KC_LCBR,    KC_LBRC,    KC_LPRN,    KC_SLSH,   KC_QUOT,                
-        KC_HASH,  KC_DLR,     KC_QUES,    KC_EXLM,    KC_PIPE,   KC_DQUO,  
+        KC_HASH,  KC_DLR,     KC_QUES,    KC_EXLM,    KC_PIPE,   KC_UNDS,  
 
-        KC_AT,    KC_NO,      KC_NO,      KC_CIRC,    KC_TILD,   KC_MINS,               
+        KC_AT,    KC_NO,      KC_NO,      KC_CIRC,    KC_TILD,   KC_DOT,               
         KC_SCLN,  KC_COLN,    KC_NO,      KC_ASTR,    KC_PERC,   KC_COMM,               
-        KC_GRV,   KC_PLUS,    MO(1),      KC_PEQL,    KC_AMPR,   KC_DOT                                                          
+        KC_GRV,   KC_PLUS,    MO(1),      KC_PEQL,    KC_AMPR,   KC_MINS                                                         
     ),
     // misc
     [2] = LAYOUT_ortho_staggered(
@@ -47,9 +47,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ARNG,  KC_UDIA,    KC_ODIA,                  KC_ADIA,        KC_NO,     KC_NO,                        
         KC_NO,    KC_NO,      "QK_MACRO_2",             "QK_MACRO_3",   KC_NO,     KC_NO, 
 
-        KC_NO,    KC_NO,      KC_NO,      KC_NO,      KC_NO,     KC_NO,
+        KC_NO,    KC_NO,      KC_NO,      KC_NO,      KC_NO,     "QK_MACRO_1",
         KC_NO,    KC_NO,      KC_NO,      KC_NO,      KC_NO,     "QK_MACRO_0", 
-        KC_NO,    MO(2),      KC_NO,      KC_NO,      KC_NO,     "QK_MACRO_1"
+        KC_NO,    MO(2),      KC_NO,      KC_NO,      KC_NO,     KC_NO
     ),
     // Nav
     [3] = LAYOUT_ortho_staggered(
@@ -57,15 +57,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LGUI,    KC_LALT,    KC_LSFT,    KC_LCTL,    KC_VOLD, MO(3),               
         C(KC_Z),    C(KC_X),    C(KC_C),    C(KC_D),    C(KC_V), KC_NO,
 
-        KC_INS,     KC_Y,       KC_PGUP,    KC_SPC,     KC_L,    KC_ESCAPE,
+        KC_INS,     KC_Y,       KC_PGUP,    KC_SPC,     KC_L,    KC_DEL,
         KC_TAB,     KC_RIGHT,   KC_UP,      KC_LEFT,    KC_PGDN, KC_BSPC,         
-        KC_ENT,     KC_H,       KC_DOWN,    KC_TAB,     KC_HOME, KC_DEL                                                     
+        KC_ENT,     KC_H,       KC_DOWN,    KC_TAB,     KC_HOME, KC_ESCAPE                                                    
     ),
     // Num
     [4] = LAYOUT_ortho_staggered(
-        KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,   KC_TAB,                                      
+        KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,   KC_ENT,                                     
         KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,  KC_SPC,                                    
-        KC_F11,   KC_F12,   KC_F13,   KC_PSCR,  KC_PAUS, KC_ENT, 
+        KC_F11,   KC_F12,   KC_F13,   KC_PSCR,  KC_PAUS, KC_TAB,  
 
         KC_BSPC,  KC_P9,    KC_P8,    KC_P7,    KC_PMNS, KC_NO,
         KC_DOT,   KC_P6,    KC_P5,    KC_P4,    KC_P0,   MO(4),
